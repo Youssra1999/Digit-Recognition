e03845c7800c2e6e834cc9ce89032953f883f6ac
Youssra1999 <100140863+Youssra1999@users.noreply.github.com>
2024-04-24 23:28:30 +0100

    linear regression and test error well done

--- part1/linear_regression.py
+++ part1/linear_regression.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-### Functions for you to fill in ###
-
 def closed_form(X, Y, lambda_factor):
     """
     Computes the closed form solution of linear regression with L2 regularization
@@ -15,13 +13,14 @@ def closed_form(X, Y, lambda_factor):
         theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
         represents the y-axis intercept of the model and therefore X[0] = 1
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    I = np.identity(X.shape[1])
+    theta = np.linalg.inv(X.T @ X + lambda_factor * I) @ X.T @ Y
+    return theta
 
-### Functions which are already complete, for you to use ###
 
 def compute_test_error_linear(test_x, Y, theta):
     test_y_predict = np.round(np.dot(test_x, theta))
     test_y_predict[test_y_predict < 0] = 0
     test_y_predict[test_y_predict > 9] = 9
     return 1 - np.mean(test_y_predict == Y)
+
--- part1/main.py
+++ part1/main.py
@@ -22,10 +22,7 @@ plot_images(train_x[0:20, :])
 # 2. Linear Regression with Closed Form Solution
 #######################################################################
 
-# TODO: first fill out functions in linear_regression.py, otherwise the functions below will not work
-
-
-def run_linear_regression_on_MNIST(lambda_factor=1):
+def run_linear_regression_on_MNIST(lambda_factor):
     """
     Trains linear regression, classifies test data, computes test error on test set
 
@@ -41,14 +38,29 @@ def run_linear_regression_on_MNIST(lambda_factor=1):
 
 
 # Don't run this until the relevant functions in linear_regression.py have been fully implemented.
-print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=1))
+print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=0.1))
+print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=0.01))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #######################################################################
 # 3. Support Vector Machine
 #######################################################################
 
-# TODO: first fill out functions in svm.py, or the functions below will not work
 
 def run_svm_one_vs_rest_on_MNIST():
     """
